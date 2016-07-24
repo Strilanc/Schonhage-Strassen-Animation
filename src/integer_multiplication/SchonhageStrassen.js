e058429b8ebfe60022d72dc2f3e27ade98111c39
Craig Gidney <craig.gidney@gmail.com>
2016-07-24 17:08:17 -0400

    Passing tests

--- src/integer_multiplication/SchonhageStrassen.js
+++ src/integer_multiplication/SchonhageStrassen.js
@@ -13,11 +13,14 @@ import {ceil_lg2, floor_lg2} from "src/util/util.js"
 function multiply_SchonhageStrassen(a, b) {
     a = BigInt.of(a);
     b = BigInt.of(b);
-    let n = Math.max(a.size(), b.size());
-    if (n <= 32) {
-        return multiply_integer_Karatsuba(a, b);
+    if (a.isNegative()) {
+        return multiply_SchonhageStrassen(a.negate(), b).negate();
     }
-    let ring = ring_for_multiplying_integers_of_size(n);
+    if (b.isNegative()) {
+        return multiply_SchonhageStrassen(a, b.negate()).negate();
+    }
+    let n = Math.max(a.size(), b.size()) * 2;
+    let ring = new FermatRing(ceil_lg2(n), 1);
     return multiply_SchonhageStrassen_ring(a, b, ring);
 }
 
@@ -35,7 +38,7 @@ function multiply_SchonhageStrassen_ring(a, b, ring) {
     b = ring.canonicalize(b);
 
     // Base cases.
-    if (ring.bit_capacity <= 64) {
+    if (ring.bit_capacity <= 8) {
         // The sub-multiplications wouldn't be smaller. Have to use a different strategy.
         return ring.canonicalize(multiply_integer_Karatsuba(a, b));
     }
@@ -44,19 +47,16 @@ function multiply_SchonhageStrassen_ring(a, b, ring) {
     if (ring.isCongruentToNegativeOne(b)) { return ring.canonicalize(a.negate()); }
 
     // Split into pieces.
-    let {inner_ring, piece_count_exponent} = sub_parameters_for_multiplying_values_in_ring(ring);
-    let bits_per_piece = ring.bit_capacity >> piece_count_exponent;
-    let piece_count = 1 << piece_count_exponent;
+    let inner_ring = ring_for_size(ring.bit_capacity);
+    let piece_count = inner_ring.principal_root_order;
+    let bits_per_piece = ring.bit_capacity / piece_count;
     let piecesA = a.splitIntoNPiecesOfSize(piece_count, bits_per_piece);
     let piecesB = b.splitIntoNPiecesOfSize(piece_count, bits_per_piece);
 
-    let d = inner_ring.principal_root_exponent - piece_count_exponent + 1;
-    let actual_inner_ring = new FermatRing(inner_ring.principal_root_exponent - d, inner_ring.bit_padding_factor << d);
-
     // Convolve pieces via fft.
-    let inner_multiply = (a, b) => multiply_SchonhageStrassen_ring(a, b, inner_ring);
-    //let inner_multiply = (a, b) => inner_ring.canonicalize(multiply_integer_Karatsuba(a, b));
-    let piecesC = actual_inner_ring.negacyclic_convolution(piecesA, piecesB, inner_multiply);
+    //let inner_multiply = (a, b) => multiply_SchonhageStrassen_ring(a, b, inner_ring);
+    let inner_multiply = (a, b) => inner_ring.canonicalize(multiply_integer_Karatsuba(a, b));
+    let piecesC = inner_ring.negacyclic_convolution(piecesA, piecesB, inner_multiply);
 
     // Perform carries.
     let combined = BigInt.shiftSum(piecesC, bits_per_piece);
@@ -66,20 +66,25 @@ function multiply_SchonhageStrassen_ring(a, b, ring) {
 /**
  * @param {!int} bit_size
  * @returns {!FermatRing}
+ * @complexity O(lg^2 n)
  */
 function ring_for_size(bit_size) {
-    let s = ceil_lg2(bit_size);
-    while (s >= 0) {
-        let p = Math.ceil(bit_size / (1<<s));
-        if (p >= 2 && s <= p && p <= 2*s) {
-            let c = (p-1) * (1 << s);
-            if (c < bit_size && c*(2<<s) >= bit_size + s + 1) {
-                return new FermatRing(s, p - 1);
+    let m = ceil_lg2(bit_size);
+    let best = undefined;
+    for (let s = m; s > 0; s--) {
+        for (let p = Math.max(2, s); p <= 2*s; p++) {
+            let r = new FermatRing(s, p - 1);
+            let isSmaller = r.bit_capacity < bit_size;
+            let hasRoom = r.bit_capacity >= Math.ceil(bit_size/r.principal_root_order)*2 + s + 1;
+            if (isSmaller && hasRoom && (best === undefined || best.r.bit_capacity > r.bit_capacity)) {
+                best = {p, s, r};
             }
         }
-        s -= 1;
     }
-    throw new Error("I can't even count that low." + bit_size)
+    if (best === undefined) {
+        throw new Error("Failed to find ring for size " + bit_size)
+    }
+    return best.r;
 }
 
 /**
--- test/integer_multiplication/SchonhageStrassen.test.js
+++ test/integer_multiplication/SchonhageStrassen.test.js
@@ -12,82 +12,67 @@ import {
 
 let suite = new Suite("integer_multiplication.SchönhageStrassen");
 
-//suite.test("auto", () => {
-//    testAllSmallCases(multiply_SchonhageStrassen);
-//    testRandomCasesAgainstNative(multiply_SchonhageStrassen, 10);
-//    testRandomLargeCasesAgainst(multiply_SchonhageStrassen, 100, multiply_integer_Karatsuba);
-//    testRandomLargeCasesAgainst(multiply_SchonhageStrassen, multiply_integer_Karatsuba, 200, 1);
-//    testRandomLargeCasesAgainst(multiply_SchonhageStrassen, multiply_integer_Karatsuba, 1000, 1);
-//});
+suite.test("auto", () => {
+    testAllSmallCases(multiply_SchonhageStrassen);
+    testRandomCasesAgainstNative(multiply_SchonhageStrassen, 10);
+    testRandomLargeCasesAgainst(multiply_SchonhageStrassen, 100, multiply_integer_Karatsuba);
+    testRandomLargeCasesAgainst(multiply_SchonhageStrassen, multiply_integer_Karatsuba, 200, 1);
+    testRandomLargeCasesAgainst(multiply_SchonhageStrassen, multiply_integer_Karatsuba, 1000, 1);
+});
 
-//suite.test("examples", () => {
-//    assertThat(multiply_SchonhageStrassen(12345, 67890)).isEqualTo(838102050);
-//    assertThat(multiply_SchonhageStrassen(123456789, 987654321)).isEqualTo(BigInt.parse("121932631112635269"));
-//});
-
-//suite.test("SchönhageStrassen_inFermatRing", () => {
-//    assertThat(multiply_SchonhageStrassen(16, 16, new FermatRing(3))).isEqualTo((16*16) % 257);
-//    assertThat(multiply_SchonhageStrassen(37, 186, new FermatRing(3))).isEqualTo((37*186) % 257);
-//    assertThat(multiply_SchonhageStrassen(53476, 15792, new FermatRing(4))).isEqualTo((53476*15792) % ((1<<16) + 1));
-//
-//    let ring = new FermatRing(4);
-//    let inRing_Karatsuba = (a, b) => ring.canonicalize(multiply_integer_Karatsuba(a, b));
-//    let inRing_SchonhageStrassen = (a, b) => multiply_SchonhageStrassen(a, b, ring);
-//    testRandomLargeCasesAgainst(inRing_SchonhageStrassen, inRing_Karatsuba, 1<<4, 2);
-//});
-
-//suite.test("SchönhageStrassen_inTheIntegers", () => {
-//    testAllSmallCases(multiply_SchonhageStrassen);
-//    testRandomCasesAgainstNative(multiply_SchonhageStrassen, 5);
-//    testRandomLargeCasesAgainst(multiply_SchonhageStrassen, multiply_integer_Karatsuba, 100, 2);
-//    testRandomLargeCasesAgainst(multiply_SchonhageStrassen, multiply_integer_Karatsuba, 200, 1);
-//});
+suite.test("examples", () => {
+    assertThat(multiply_SchonhageStrassen(12345, 67890)).isEqualTo(838102050);
+    assertThat(multiply_SchonhageStrassen(123456789, 987654321)).isEqualTo(BigInt.parse("121932631112635269"));
+});
 
 suite.test("ring_for_size", () => {
     assertThrows(() => ring_for_size(0));
     assertThrows(() => ring_for_size(1));
     assertThrows(() => ring_for_size(2));
-    assertThat(ring_for_size(3)).isEqualTo(new FermatRing(1, 1));
-    assertThat(ring_for_size(4)).isEqualTo(new FermatRing(1, 1));
-
-    assertThat(ring_for_size(5)).isEqualTo(new FermatRing(2, 1));
-    assertThat(ring_for_size(6)).isEqualTo(new FermatRing(2, 1));
-    assertThat(ring_for_size(7)).isEqualTo(new FermatRing(2, 1));
-    assertThat(ring_for_size(8)).isEqualTo(new FermatRing(2, 1));
+    assertThrows(() => ring_for_size(3));
+    assertThrows(() => ring_for_size(4));
+    assertThrows(() => ring_for_size(5));
+    assertThrows(() => ring_for_size(6));
+    assertThrows(() => ring_for_size(7));
+    assertThrows(() => ring_for_size(8));
 
     assertThat(ring_for_size(9)).isEqualTo(new FermatRing(2, 2));
     assertThat(ring_for_size(10)).isEqualTo(new FermatRing(2, 2));
     assertThat(ring_for_size(11)).isEqualTo(new FermatRing(2, 2));
     assertThat(ring_for_size(12)).isEqualTo(new FermatRing(2, 2));
-    assertThat(ring_for_size(13)).isEqualTo(new FermatRing(2, 3));
-    assertThat(ring_for_size(14)).isEqualTo(new FermatRing(2, 3));
-    assertThat(ring_for_size(15)).isEqualTo(new FermatRing(2, 3));
-    assertThat(ring_for_size(16)).isEqualTo(new FermatRing(2, 3));
+    assertThat(ring_for_size(13)).isEqualTo(new FermatRing(2, 2));
+    assertThat(ring_for_size(14)).isEqualTo(new FermatRing(2, 2));
+    assertThat(ring_for_size(15)).isEqualTo(new FermatRing(2, 2));
+    assertThat(ring_for_size(16)).isEqualTo(new FermatRing(2, 2));
 
-    assertThat(ring_for_size(17)).isEqualTo(new FermatRing(3, 2));
-    assertThat(ring_for_size(18)).isEqualTo(new FermatRing(3, 2));
-    assertThat(ring_for_size(19)).isEqualTo(new FermatRing(3, 2));
-    assertThat(ring_for_size(20)).isEqualTo(new FermatRing(3, 2));
-    assertThat(ring_for_size(21)).isEqualTo(new FermatRing(3, 2));
-    assertThat(ring_for_size(22)).isEqualTo(new FermatRing(3, 2));
-    assertThat(ring_for_size(23)).isEqualTo(new FermatRing(3, 2));
-    assertThat(ring_for_size(24)).isEqualTo(new FermatRing(3, 2));
-    assertThat(ring_for_size(25)).isEqualTo(new FermatRing(3, 3));
-    assertThat(ring_for_size(26)).isEqualTo(new FermatRing(3, 3));
-    assertThat(ring_for_size(27)).isEqualTo(new FermatRing(3, 3));
-    assertThat(ring_for_size(28)).isEqualTo(new FermatRing(3, 3));
-    assertThat(ring_for_size(29)).isEqualTo(new FermatRing(3, 3));
-    assertThat(ring_for_size(30)).isEqualTo(new FermatRing(3, 3));
-    assertThat(ring_for_size(31)).isEqualTo(new FermatRing(3, 3));
-    assertThat(ring_for_size(32)).isEqualTo(new FermatRing(3, 3));
+    assertThat(ring_for_size(17)).isEqualTo(new FermatRing(2, 3));
+    assertThat(ring_for_size(18)).isEqualTo(new FermatRing(2, 3));
+    assertThat(ring_for_size(19)).isEqualTo(new FermatRing(2, 3));
+    assertThat(ring_for_size(20)).isEqualTo(new FermatRing(2, 3));
+    assertThat(ring_for_size(21)).isEqualTo(new FermatRing(2, 3));
+    assertThat(ring_for_size(22)).isEqualTo(new FermatRing(2, 3));
+    assertThat(ring_for_size(23)).isEqualTo(new FermatRing(2, 3));
+    assertThat(ring_for_size(24)).isEqualTo(new FermatRing(2, 3));
+    assertThat(ring_for_size(25)).isEqualTo(new FermatRing(2, 3));
+    assertThat(ring_for_size(26)).isEqualTo(new FermatRing(2, 3));
+    assertThat(ring_for_size(27)).isEqualTo(new FermatRing(2, 3));
+    assertThat(ring_for_size(28)).isEqualTo(new FermatRing(2, 3));
+    assertThat(ring_for_size(29)).isEqualTo(new FermatRing(2, 3));
+    assertThat(ring_for_size(30)).isEqualTo(new FermatRing(2, 3));
+    assertThat(ring_for_size(31)).isEqualTo(new FermatRing(2, 3));
+    assertThat(ring_for_size(32)).isEqualTo(new FermatRing(2, 3));
 
-    assertThat(ring_for_size(33)).isEqualTo(new FermatRing(3, 4));
-    //assertThat(ring_for_size(100)).isEqualTo(new FermatRing(4, 6));
-    //assertThat(ring_for_size(1000)).isEqualTo(new FermatRing(7, 7));
-    //assertThat(ring_for_size(10000)).isEqualTo(new FermatRing(10, 9));
-    //assertThat(ring_for_size(100000)).isEqualTo(new FermatRing(13, 12));
-    //assertThat(ring_for_size(1000000)).isEqualTo(new FermatRing(16, 15));
-    //assertThat(ring_for_size(10000000)).isEqualTo(new FermatRing(19, 19));
-    //assertThat(ring_for_size(100000000)).isEqualTo(new FermatRing(19, 19));
-    //assertThat(ring_for_size(10000000)).isEqualTo(new FermatRing(19, 19));
+    assertThat(ring_for_size(33)).isEqualTo(new FermatRing(3, 2));
+    assertThat(ring_for_size(34)).isEqualTo(new FermatRing(3, 2));
+    assertThat(ring_for_size(35)).isEqualTo(new FermatRing(3, 2));
+
+    assertThat(ring_for_size(100)).isEqualTo(new FermatRing(3, 3));
+    assertThat(ring_for_size(1000)).isEqualTo(new FermatRing(4, 5));
+    assertThat(ring_for_size(10000)).isEqualTo(new FermatRing(6, 5));
+    assertThat(ring_for_size(100000)).isEqualTo(new FermatRing(7, 7));
+    assertThat(ring_for_size(1000000)).isEqualTo(new FermatRing(9, 8));
+    assertThat(ring_for_size(10000000)).isEqualTo(new FermatRing(10, 10));
+    assertThat(ring_for_size(100000000)).isEqualTo(new FermatRing(12, 11));
+    assertThat(ring_for_size(1000000000)).isEqualTo(new FermatRing(13, 15));
+    assertThat(ring_for_size(10000000000)).isEqualTo(new FermatRing(15, 14));
 });
